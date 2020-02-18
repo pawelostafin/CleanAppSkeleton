34d7f0766df35fe140b8ef437a541c96ca1abe19
pawelostafin <pawelostafindev@gmail.com>
2020-02-18 20:07:22 +0100

    buildSrcVersions added

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,9 @@
+object Modules {
+    const val app = ":app"
+    const val common = ":common"
+    const val data = ":data"
+    const val domain = ":domain"
+    const val mainscreen = ":mainscreen"
+    const val secondscreen = ":secondscreen"
+    const val usecases = ":usecases"
+}
